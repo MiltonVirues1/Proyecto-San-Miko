7561b36d314b97b63cf3ddefc6ef0dd9093b4c83
xXala011 <xxala01@gmail.com>
2025-06-29 05:26:48 -0300

    Primer version de miko 0.1

---
+++ san_miko_test1.py
@@ -0,0 +1,44 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import os
+
+# Inicializar motor de voz
+voz = pyttsx3.init()
+voz.setProperty('rate', 150)
+
+def hablar(texto):
+    print("San Miko:", texto)
+    voz.say(texto)
+    voz.runAndWait()
+
+def escuchar():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Escuchando...")
+        audio = r.listen(source)
+
+    try:
+        comando = r.recognize_google(audio, language='es-AR')
+        print("Usuario:", comando)
+        return comando.lower()
+    except:
+        hablar("Perdón, no entendí.")
+        return ""
+
+# Comienzo
+hablar("Hola Milton, soy San Miko. Te escucho.")
+while True:
+    comando = escuchar()
+
+    if "abrí música" in comando or "abrir música" in comando:
+     hablar("Abriendo YouTube Music en el navegador.")
+     webbrowser.open("https://music.youtube.com/")
+    elif "reproduci algo" in comando or "Iniciar música" in comando:
+     hablar("Entendido milton, ya la reproduzco pa.")
+     webbrowser.open("https://music.youtube.com/watch?v=APYazXm6lH0&si=-KvMwprC4zLQ7-vS") 
+    elif "chau" in comando or "chao" in comando or "cerrar" in comando:
+     hablar("Hasta luego, Milton.")
+     break
+    elif comando != "":
+        hablar("Ese comando aún no lo conozco.")
